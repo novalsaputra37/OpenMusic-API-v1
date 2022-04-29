008455e78ca7a1d73dff3613f76653579e3b1d24
novalsaputra37@gmail.com <novalsaputra37@gmail.com>
2022-04-29 19:46:15 +0700

    openMusic v2 new

--- migrations/1651156882898_add-foreign-key-to-albumId-column.js
+++ migrations/1651156882898_add-foreign-key-to-albumId-column.js
@@ -4,8 +4,8 @@ exports.shorthands = undefined;
 
 exports.up = (pgm) => {
     pgm.sql("INSERT INTO albums(id, name, year) VALUES ('old_songs', 'old_songs', 2022)");
-    pgm.sql("UPDATE songs SET 'albumId' = 'old_songs' WHERE 'albumId' = NULL");
-    pgm.addConstraint('songs', 'fk_songs.albumId_songs.id', 'FOREIGN KEY(albumId) REFERENCES albums(id) ON DELETE CASCADE');
+    pgm.sql('UPDATE songs SET "albumId" = \'old_songs\' WHERE "albumId" = NULL');
+    pgm.addConstraint('songs', 'fk_songs.albumId_songs.id', 'FOREIGN KEY("albumId") REFERENCES albums(id) ON DELETE CASCADE');
 };
 
 exports.down = (pgm) => {
